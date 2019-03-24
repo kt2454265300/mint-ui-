fa66a3f9ade1c9cfaf6d0efb880fd6a4b0f94dbb
kt <2454265300@qq.com>
2019-03-24 13:32:07 +0800

    项目开始

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <!-- 顶部区域header -->
+        <mt-header fixed title="练习项目"></mt-header>
+
+     <!-- 中间部分 -->
+
+     <!-- 底部区域 -->
+
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
